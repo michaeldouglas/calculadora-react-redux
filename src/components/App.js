3d99b2f71dfdbe22bfe03664f975a3e0cec064f5
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-02-13 22:30:47 -0200

    Ajustes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
+import Soma from './Somar'
 
 import './css/calculadora.css'
-
-//Containers
-import Somar from './../containers/SomarValor'
-import ResultadoDaSoma from './../containers/Resultado'
 
 class App extends Component {
   render() {
     return (
-      <div className="bgimg-1 box">
-        <p className="title">Somar</p>
-        <ResultadoDaSoma />
-        <Somar />
+      <div className="box">
+        <Soma />
       </div>
     )
   }
