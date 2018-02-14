2867fb66c289973b779553402fda223df0336e7d
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-02-13 23:34:16 -0200

    Ajustes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import Soma from './Somar'
+import { Route } from 'react-router-dom'
+
+import Calculadora from './Calculadora'
+import Menu from './Menu'
 
 import './css/calculadora.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="box">
-        <Soma />
+      <div>
+        <Menu />
+        <section className="box">
+          <Route exact path="/" render={() => (
+            <Calculadora
+              title="Somar"
+            />
+          )}/>
+          <Route exact path="/subtrair" render={() => (
+            <Calculadora
+              title="Subtrair"
+            />
+          )}/>
+          <Route exact path="/dividir" render={() => (
+            <Calculadora
+              title="Dividir"
+            />
+          )}/>
+          <Route exact path="/multiplicar" render={() => (
+            <Calculadora
+              title="Multiplicar"
+            />
+          )}/>
+        </section>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
 import appReducer from './reducers'
+
+import 'font-awesome/css/font-awesome.min.css';
 
 let store = createStore(
   appReducer,
@@ -11,8 +14,10 @@ let store = createStore(
 )
 
 render(
-  <Provider store={store}>
-    <App />
-    </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
